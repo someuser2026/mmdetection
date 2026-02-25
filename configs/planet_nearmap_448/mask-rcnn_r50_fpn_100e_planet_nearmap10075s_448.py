fb2fb52a44ee4dc15596d815cca27610b0e26ea5
someuser2026 <forgithub2026@gmail.com>
2026-02-26 08:37:31 +1100

    Troubleshoot training config error

--- configs/planet_nearmap_448/mask-rcnn_r50_fpn_100e_planet_nearmap10075s_448.py
+++ configs/planet_nearmap_448/mask-rcnn_r50_fpn_100e_planet_nearmap10075s_448.py
@@ -7,5 +7,25 @@ _base_ = [
 
 model = dict(
     roi_head=dict(
-        bbox_head=dict(num_classes=1),
-        mask_head=dict(num_classes=1)))
+        bbox_head=dict(
+            type='Shared2FCBBoxHead',
+            in_channels=256,
+            fc_out_channels=1024,
+            roi_feat_size=7,
+            num_classes=1,
+            bbox_coder=dict(
+                type='DeltaXYWHBBoxCoder',
+                target_means=[0., 0., 0., 0.],
+                target_stds=[0.1, 0.1, 0.2, 0.2]),
+            reg_class_agnostic=False,
+            loss_cls=dict(
+                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
+            loss_bbox=dict(type='L1Loss', loss_weight=1.0)),
+        mask_head=dict(
+            type='FCNMaskHead',
+            num_convs=4,
+            in_channels=256,
+            conv_out_channels=256,
+            num_classes=1,
+            loss_mask=dict(
+                type='CrossEntropyLoss', use_mask=True, loss_weight=1.0))))
--- configs/planet_nearmap_448/retinanet_r50_fpn_100e_planet_nearmap10075s_448.py
+++ configs/planet_nearmap_448/retinanet_r50_fpn_100e_planet_nearmap10075s_448.py
@@ -6,7 +6,29 @@ _base_ = [
 ]
 
 model = dict(
-    bbox_head=dict(num_classes=1),
+    bbox_head=dict(
+        type='RetinaHead',
+        num_classes=1,
+        in_channels=256,
+        stacked_convs=4,
+        feat_channels=256,
+        anchor_generator=dict(
+            type='AnchorGenerator',
+            octave_base_scale=4,
+            scales_per_octave=3,
+            ratios=[0.5, 1.0, 2.0],
+            strides=[8, 16, 32, 64, 128]),
+        bbox_coder=dict(
+            type='DeltaXYWHBBoxCoder',
+            target_means=[.0, .0, .0, .0],
+            target_stds=[1.0, 1.0, 1.0, 1.0]),
+        loss_cls=dict(
+            type='FocalLoss',
+            use_sigmoid=True,
+            gamma=2.0,
+            alpha=0.25,
+            loss_weight=1.0),
+        loss_bbox=dict(type='L1Loss', loss_weight=1.0)),
 )
 
 # RetinaNet is a bbox-only detector.
