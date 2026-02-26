0fb71340e45793855f3ed0a14254087f0ce68975
someuser2026 <forgithub2026@gmail.com>
2026-02-26 13:16:25 +1100

    Enable segm mAP best checkpoint

--- configs/_base_/default_runtime_planet.py
+++ configs/_base_/default_runtime_planet.py
@@ -4,7 +4,12 @@ default_hooks = dict(
     timer=dict(type='IterTimerHook'),
     logger=dict(type='LoggerHook', interval=50),
     param_scheduler=dict(type='ParamSchedulerHook'),
-    checkpoint=dict(type='CheckpointHook', interval=1, max_keep_ckpts=1),
+    checkpoint=dict(
+        type='CheckpointHook',
+        interval=1,
+        max_keep_ckpts=1,
+        save_best='segm_mAP_50',
+        rule='greater'),
     sampler_seed=dict(type='DistSamplerSeedHook'),
     visualization=dict(type='DetVisualizationHook'))
 
